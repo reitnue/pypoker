5c7f981f46ecca243c752b420fe4bab599a1dec1
Alex Wang <lifengawang@gmail.com>
2020-01-15 15:32:06 -0500

    Poker parent class

---
+++ src/NLHE.py
@@ -0,0 +1,68 @@
+import sys
+
+from deck import Deck
+from poker import Poker
+'''
+1. Game Play first
+2. Betting
+'''
+
+# Hold 'em
+# Max 9-Handed
+class NLHE(Poker):
+    '''
+    players: list of player objects - starts with small blind...
+    '''
+    def __init__(self, players, sb=1, bb=2):
+        super().__init__(players)
+        self.community = []
+        
+        self.cards_per_hand = 2
+
+        self.small_blind = sb
+        self.big_blind = bb
+
+    # '''
+    # player: id for player for self.hands
+    # community: community cards
+    # '''
+    # def score_hand(self, player):
+    #     pass
+
+    def play(self):
+        # deal hands
+        for i in range(self.num_players):
+            for _ in range(self.cards_per_hand):
+                self.hands[i].append(self.deck.deal())
+
+        # flop
+        for _ in range(3):
+            self.community.append(self.deck.deal())
+        # turn
+        for _ in range(1):
+            self.community.append(self.deck.deal())
+        # river
+        for _ in range(1):
+            self.community.append(self.deck.deal())
+
+        return
+
+    def show_community(self):
+        for i in range(5):
+            if len(self.community) > i:
+                sys.stdout.write(str(self.community[i])+" ")
+            else:
+                sys.stdout.write('___ ')
+        print('')
+
+    def show_hands(self):
+        for i in range(len(self.players)):
+            print(i, self.hands[i][0], self.hands[i][1])
+
+        return
+
+if __name__ == '__main__':
+    game = NLHE(list(range(2)))
+    game.play()
+    game.show_hands()
+    game.show_community()
--- src/poker.py
+++ src/poker.py
@@ -1,34 +1,31 @@
+import sys
+
 from deck import Deck
+
+'''
+1. Game Play first
+2. Betting
+'''
 
 # Hold 'em
 # Max 9-Handed
-class NLHE():
-	def __init__(self, sb=1, bb=2):
-		self.deck = Deck()
-		self.deck.shuffle()
+class Poker():
+    '''
+    players: list of player objects - starts with small blind...
+    '''
+    def __init__(self, players):
+        self.deck = Deck()
+        self.deck.shuffle()
 
-		self.hands = []
-		self.community = []
-		
-		self.cards_per_hand = 2
-		self.players = []
-		self.num_players = 0
-		self.pot = 0
-		self.small_blind = sb
-		self.big_blind = bb
+        self.players = players
+        self.num_players = len(players)
 
-	
-	def add_player(self, player):
-		if self.num_players == 9:
-			return False
-		self.players.append(player)
-		self.num_players += 1
-		return True
+        self.hands = [[] for _ in players]
+        self.folded = [0 for _ in players]
 
+        self.pot = 0
 
-	'''
-	player: id for player for self.hands
-	community: community cards
-	'''
-	def score_hand(self, player):
-		pass
+    def score_hand(self):
+        pass
+
+    # def 
